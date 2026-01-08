9242f6089f7f425ec2a63965e5f36b3f415a7f73
luv <muyogalega@gmail.com>
2026-01-08 16:18:01 -0600

    updated output.tf file

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,19 +1,24 @@
 output "monitoring_public_ip" {
-  value = aws_instance.monitoring.public_ip
+  description = "Public IP of the monitoring server"
+  value       = aws_instance.monitoring.public_ip
 }
 
 output "prometheus_url" {
-  value = "http://${aws_instance.monitoring.public_ip}:9090"
+  description = "Prometheus UI URL"
+  value       = "http://${aws_instance.monitoring.public_ip}:9090"
 }
 
 output "grafana_url" {
-  value = "http://${aws_instance.monitoring.public_ip}:3000"
+  description = "Grafana UI URL"
+  value       = "http://${aws_instance.monitoring.public_ip}:3000"
 }
 
 output "alertmanager_url" {
-  value = "http://${aws_instance.monitoring.public_ip}:9093"
+  description = "Alertmanager UI URL"
+  value       = "http://${aws_instance.monitoring.public_ip}:9093"
 }
 
-output "app_private_ip" {
-  value = aws_instance.app.private_ip
+output "app_private_ips" {
+  description = "Private IPs of all app servers"
+  value       = aws_instance.app[*].private_ip
 }
